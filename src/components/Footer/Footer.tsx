16d5cc41c42ef4dab6da029ac51a6b4861ef0b04
Heraldo Fortuna <heraldofortuna@gmail.com>
2025-07-04 14:55:09 -0500

    implement sass with bem method

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,13 @@
-import styles from './Footer.module.scss';
 import RimacLogo from '@assets/logo-white.svg';
 import RimacMobileLogo from '@assets/logo-mobile-white.svg';
+import styles from './Footer.module.scss';
 
 const Footer: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.container}>
-        <img src={RimacMobileLogo} alt="Mobile Rimac Logo" className={styles.logoMobile} />
-        <img src={RimacLogo} alt="Desktop Rimac Logo" className={styles.logoDesktop} />
+        <img src={RimacMobileLogo} alt="Mobile Rimac Logo" className={styles['logo--mobile']} />
+        <img src={RimacLogo} alt="Desktop Rimac Logo" className={styles['logo--desktop']} />
         <span className={styles.separator}></span>
         <p className={styles.text}>© 2023 RIMAC Seguros y Reaseguros.</p>
       </div>
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,22 @@
+import RimacLogo from '@assets/logo-red.svg';
+import PhoneIcon from '@assets/phone.svg';
 import styles from './Header.module.scss';
 
 const Header: React.FC = () => {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Mi App</h1>
+      <div className={styles.container}>
+        <img src={RimacLogo} alt="Rimac Logo" className={styles.logo} />
+        <div className={styles.contact}>
+          <p className={styles.contact__text}>¡Compra por este medio!</p>
+          <div className={styles.contact__phone}>
+            <span className={styles.contact__phone__icon}>
+              <img src={PhoneIcon} alt="(01) 411 6001" />
+            </span>
+            <p className={styles.contact__phone__text}>(01) 411 6001</p>
+          </div>
+        </div>
+      </div>
     </header>
   );
 };
